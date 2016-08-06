a5d174eb20f11064f8a31b6170aec28ae9fe0c20
Francois Guibert <frozax@frozax.com>
2016-08-06 22:16:38 +0200

    curl and wget

--- get.py
+++ get.py
@@ -4,8 +4,9 @@ file_with_raw_links = "../../dl.txt.txt"
 for line in open(file_with_raw_links).readlines():
     line = line.strip()
     dest_name = line.split('/')[-1].split('?')[0]
-    cmd = "curl -o %s \"%s\"" % (dest_name, line)
-    os.system(cmd)
+    cmd_curl = "curl -o %s \"%s\"" % (dest_name, line)
+    cmd_wget = "wget -O %s \"%s\"" % (dest_name, line)
+    os.system(cmd_wget)
     os.system("gunzip %s" % dest_name)
     break
 
