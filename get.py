a6a6d0084a593d0218ea443adffe83358f05c984
Francois Guibert <frozax@frozax.com>
2016-08-06 22:17:43 +0200

    remove test break

--- get.py
+++ get.py
@@ -9,6 +9,3 @@ for line in open(file_with_raw_links).readlines():
     cmd_wget = "wget -O %s \"%s\"" % (dest_name, line)
     os.system(cmd_wget)
     os.system("gunzip %s" % dest_name)
-    break
-
-
