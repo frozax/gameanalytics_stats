011b4ba824e3fde58d1a16826988ce1ee76a6334
Francois Guibert <frozax@frozax.com>
2016-08-09 16:05:39 +0200

    create client data from ids

--- clientdata.py
+++ clientdata.py
@@ -1,10 +1,23 @@
 import os
+import json
 
 class ClientData:
+    def __init__(self, client_id):
+        self.id = client_id
+
     def raw(self):
-        return "cd"
+        return "{id: %s}" % (self.id)
 
 
 def client_data_generator(file_with_client_data):
+    l = []
+    cur_cd = None
     for line in open(file_with_client_data):
-        yield ClientData()
+        parsed = json.loads(line)
+        client_id, ts, event, version, session_num = parsed
+        if cur_cd is None or cur_cd.id != client_id:
+            if cur_cd:
+                yield cur_cd
+            cur_cd = ClientData(client_id)
+    if cur_cd:
+        yield cur_cd
