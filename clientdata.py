670325d3bf1c8100bd1baecaa4173131f2329647
Francois Guibert <frozax@frozax.com>
2016-08-14 10:25:26 +0200

    events as array

--- clientdata.py
+++ clientdata.py
@@ -1,14 +1,15 @@
 import json
+from tutorial import tutorial_stats
 
 
 class Event:
     def __init__(self, name, timestamp, session_num):
-        self.name = name
+        self.name = name.split(':')
         self.timestamp = timestamp
         self.session_num = session_num
 
     def __repr__(self):
-        return "{%s %s %s}" % (self.name, self.timestamp, self.session_num)
+        return "%s %s %s" % (self.name, self.timestamp, self.session_num)
 
 
 class ClientData:
@@ -31,6 +32,7 @@ class ClientData:
         self.events.append(event)
 
     def compute_stats(self):
+        self.stats.update(tutorial_stats(self.events))
         self.stats["test"] = 0
 
 
@@ -38,11 +40,11 @@ def client_data_generator(file_with_client_data):
     cur_cd = None
     for line in open(file_with_client_data):
         parsed = json.loads(line)
-        client_id, ts, event, version, session_num = parsed
+        client_id, ts, event_str, version, session_num = parsed
         if cur_cd is None or cur_cd.id != client_id:
             if cur_cd:
                 yield cur_cd
             cur_cd = ClientData(client_id)
-        cur_cd.add_event(Event(event, ts, session_num))
+        cur_cd.add_event(Event(event_str, ts, session_num))
     if cur_cd:
         yield cur_cd
