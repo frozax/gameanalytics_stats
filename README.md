2e282d12a206cb4e8066c26a325e0a5a531570f0
Francois Guibert <frozax@frozax.com>
2016-08-21 16:42:48 +0200

    lambda aggregate

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Scripts to extract statistics from raw game analytics date
 X tuto: exit (skip or done)
 X 2 levels completed
 - hint per level (per pack, when pack completed)
-- pack A completed depending on game update
+X pack x completed depending on game update
 X time until first purchase
 X levels completed until first purchase
 X rating
--- aggregate.py
+++ aggregate.py
@@ -1,6 +1,7 @@
-def count_by_value(res, stat):
-    """group by value of stat
+def count_by_value(res, client_stats, key):
+    """group by value of stat value stored in 'key'
     """
+    stat = client_stats.get(key, "-")
     if not res:
         res = {}
     if stat not in res:
@@ -9,12 +10,27 @@ def count_by_value(res, stat):
         res[stat] += 1
     return res
 
+def count_by_lambda(res, client_stats, func):
+    """group by value returnedby function
+    applied to stats. ignore if None
+    """
+    key = func.__name__
+    client_stats[key] = func(client_stats)
+    return count_by_value(res, client_stats, key)
+
+
+def completed_any_pack_per_version(client_stats):
+    return client_stats.get("initial_version", "-") + " " + ("yes" if client_stats.get("completed_packs", 0) > 0 else "no")
+
 
 def aggregate_cd(res, client_stats):
     if res is None:
         res = {}
-    CONFS = [("tuto_last_event", count_by_value)]
-    for key, func in CONFS:
-        res[key] = func(res.get(key), client_stats.get(key, "-"))
+    CONFS = [
+        ("tuto_last_event", count_by_value, ("tuto_last_event",)),
+        ("completed_any_pack_per_version", count_by_lambda, (completed_any_pack_per_version,))
+    ]
+    for key, func, args in CONFS:
+        res[key] = func(res.get(key), client_stats, *args)
 
     return res
