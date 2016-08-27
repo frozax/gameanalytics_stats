6a5274e9e3321d7330dc6ad200dca8f16ab3a240
Francois Guibert <frozax@frozax.com>
2016-08-28 00:21:15 +0200

    action remove ignored ids

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ subparsers = parser.add_subparsers(dest="command")
 ga_parser = subparsers.add_parser("ga-data")
 ga_parser.add_argument("ga_file")
 ga_parser.add_argument("stats_file")
+remove_ids_parser = subparsers.add_parser("remove-ids")
+remove_ids_parser.add_argument("ga_file")
+remove_ids_parser.add_argument("ids_file")
+remove_ids_parser.add_argument("ga_without_ids_file")
 stats_parser = subparsers.add_parser("stats-agg")
 stats_parser.add_argument("stats_file")
 stats_parser.add_argument("final_file")
@@ -29,6 +33,19 @@ if args.command == "ga-data":
         if nbc > 150:
             break
     fstats.close()
+elif args.command == "remove-ids":
+    ignored_ids = []
+    for ignored_id in open(args.ids_file):
+        ignored_ids.append(ignored_id.strip())
+    fga = open(args.ga_file)
+    fga_no_ids = open(args.ga_without_ids_file, "wt")
+    for line in fga:
+        parsed = json.loads(line)
+        client_id = parsed[0]
+        if client_id not in ignored_ids:
+            fga_no_ids.write(line)
+        else:
+            print("ignored " + client_id)
 elif args.command == "stats-agg":
     res = None
     for l in open(args.stats_file):
